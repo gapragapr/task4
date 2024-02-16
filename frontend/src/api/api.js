8583aa61beb183d5fa21f931f6f25718ae4b7b79
gapragapr <gapragapr3@mail.ru>
2024-02-17 00:41:13 +0600

    no-corse

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: `${import.meta.env.VITE_API_URL}` }),
+  baseQuery: fetchBaseQuery({ baseUrl: `${import.meta.env.VITE_API_URL}`, fetchOptions: {
+    mode: 'no-cors',
+  }, }),
   endpoints: (builder) => ({
     register: builder.mutation({
       query: (body) => ({
